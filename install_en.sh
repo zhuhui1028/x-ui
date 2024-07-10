0a60f2fcbd5cf183b38a892ca9dbee1589326017
zhuhui1028 <228252012@qq.com>
2024-07-10 09:54:52 +0800

    Update install_en.sh

--- install_en.sh
+++ install_en.sh
@@ -127,16 +127,16 @@ install_x-ui() {
             exit 1
         fi
         echo -e "get x-ui latest version succeed:${last_version},begin to install..."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux.tar.gz https://github.com/zhuhui1028/x-ui/releases/download/${last_version}/x-ui-linux.tar.gz
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}-english.tar.gz https://github.com/zhuhui1028/x-ui/releases/download/${last_version}/x-ui-linux-${arch}-english.tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}dowanload x-ui failed,please be sure that your server can access Github{plain}"
             exit 1
         fi
     else
         last_version=$1
-        url="https://github.com/zhuhui1028/x-ui/releases/download/${last_version}/x-ui-linux.tar.gz"
+        url="https://github.com/zhuhui1028/x-ui/releases/download/${last_version}/x-ui-linux-${arch}-english.tar.gz"
         echo -e "begin to install x-ui v$1 ..."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux.tar.gz ${url}
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}-english.tar.gz ${url}
         if [[ $? -ne 0 ]]; then
             echo -e "${red}dowanload x-ui v$1 failed,please check the verison exists${plain}"
             exit 1
@@ -147,11 +147,9 @@ install_x-ui() {
         rm /usr/local/x-ui/ -rf
     fi
 
-    mkdir x-ui
-    mv x-ui-linux.tar.gz x-ui
+    tar zxvf x-ui-linux-${arch}-english.tar.gz
+    rm x-ui-linux-${arch}-english.tar.gz -f
     cd x-ui
-    tar zxvf x-ui-linux.tar.gz
-    rm x-ui-linux.tar.gz -f
     chmod +x x-ui bin/xray-linux-${arch}
     cp -f x-ui.service /etc/systemd/system/
     wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/zhuhui1028/x-ui/main/x-ui_en.sh
